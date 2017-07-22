ef6e67cf1d1bb8f401240ac5737c5eb0928682be
James Forbes <james.a.forbes@gmail.com>
2017-07-22 17:48:04 +1000

    Remove dead code from predicated dev

--- predicated/dev.js
+++ predicated/dev.js
@@ -44,49 +44,4 @@ function PredicatedLibDev(toString, handleError){
     return SumType
 }
 
-function PredicatedLib(toString, handleError){
-
-    function SumType(name, cases){
-      
-      var type = 
-        Object.keys(cases)
-          .reduce(function(p, k){
-            var predicate = 
-              cases[k]
-
-            p[k] = function(value){
-              if( !predicate(value) ){
-                return handleError({
-                  case: k
-                  ,predicate: predicate
-                  ,value: value
-                  ,message: 'Value: '
-                    + toString(value)
-                    + ' did not satisfy the constraint for '
-                    + name+'.'+k
-                    + ': '
-                    + toString(predicate)
-                })
-              } else {
-                function instance$toString(){
-                  return name+'.'+k+'('+ (value) +')'
-                }
-                return {
-                  type: { name }
-                  ,case: { name: k }
-                  ,value: value
-                  ,toString: instance$toString
-                  ,inspect: instance$toString
-                }
-              }
-            }
-
-            return p
-        }, { name: name })
-
-      return type
-    }
-
-    return SumType
-}
 module.exports = PredicatedLibDev
